29249366a380b5ad881e426ff1367598850db612
y5nw <37980625+y5nw@users.noreply.github.com>
2025-02-23 01:17:36 +0100

    Check for keybinding conflicts

--- src/script/lua_api/l_mainmenu.cpp
+++ src/script/lua_api/l_mainmenu.cpp
@@ -21,6 +21,7 @@
 #include "settings.h"
 #include "clientdynamicinfo.h"
 #include "client/client.h"
+#include "client/keycode.h"
 #include "client/renderingengine.h"
 #include "client/texturepaths.h"
 #include "network/networkprotocol.h"
@@ -1013,6 +1014,15 @@ int ModApiMainMenu::l_share_file(lua_State *L)
 }
 
 /******************************************************************************/
+int ModApiMainMenu::l_are_keycodes_equal(lua_State *L)
+{
+	auto k1 = luaL_checkstring(L, 1);
+	auto k2 = luaL_checkstring(L, 2);
+	lua_pushboolean(L, KeyPress(k1) == KeyPress(k2));
+	return 1;
+}
+
+/******************************************************************************/
 int ModApiMainMenu::l_do_async_callback(lua_State *L)
 {
 	MainMenuScripting *script = getScriptApi<MainMenuScripting>(L);
@@ -1084,6 +1094,7 @@ void ModApiMainMenu::Initialize(lua_State *L, int top)
 	API_FCT(open_url_dialog);
 	API_FCT(open_dir);
 	API_FCT(share_file);
+	API_FCT(are_keycodes_equal);
 	API_FCT(do_async_callback);
 }
 
