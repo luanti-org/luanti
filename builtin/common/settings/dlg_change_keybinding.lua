d7193280a0d82a88a560761263187724c23b4144
y5nw <37980625+y5nw@users.noreply.github.com>
2025-04-01 23:53:24 +0200

    Make dlg_change_keybinding work in-game

--- builtin/common/settings/dlg_change_keybinding.lua
+++ builtin/common/settings/dlg_change_keybinding.lua
@@ -97,11 +97,56 @@ local function buttonhandler(self, fields)
 	return false
 end
 
-function create_change_keybinding_dlg(setting)
-	local retval = dialog_create("dlg_change_keybinding",
-			get_formspec, buttonhandler)
-	retval.data.setting = setting
-	return retval
+local formspec_handlers = {}
+if INIT == "pause_menu" then
+	core.register_on_formspec_input(function(formname, fields)
+		if formspec_handlers[formname] then
+			formspec_handlers[formname](fields)
+			return true
+		end
+	end)
+end
+
+local is_mainmenu = INIT == "mainmenu"
+function show_change_keybinding_dlg(setting, tabview)
+	local dlg
+	if is_mainmenu then
+		dlg = dialog_create("dlg_change_keybinding",
+				get_formspec, buttonhandler)
+	else
+		local name = "__builtin:rebind_" .. setting.name
+		dlg = {
+			show = function()
+				if dlg.removed then
+					--core.open_settings("controls_keyboard_and_mouse")
+					tabview:show()
+				else
+					core.show_formspec(name, get_formspec(dlg.data))
+				end
+			end,
+			delete = function()
+				core.show_formspec(name, "")
+				formspec_handlers[name] = nil
+				dlg.removed = true
+			end,
+			data = {},
+		}
+		formspec_handlers[name] = function(fields)
+			if buttonhandler(dlg, fields) then
+				dlg:show()
+			end
+		end
+	end
+
+	dlg.data.setting = setting
+
+	if is_mainmenu then
+		dlg:set_parent(tabview)
+		tabview:hide()
+	end
+	dlg:show()
+
+	return is_mainmenu
 end
 
 -- This is also used by the setting component to display conflict warnings in the setting menu
