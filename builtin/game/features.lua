6b95c95ce9c0e0c4029c20a898499b1a945abb17
Lars Müller <34514239+appgurueu@users.noreply.github.com>
2025-04-19 18:40:33 +0200

    .

--- builtin/game/features.lua
+++ builtin/game/features.lua
@@ -47,7 +47,7 @@ core.features = {
 	particle_blend_clip = true,
 	remove_item_match_meta = true,
 	httpfetch_additional_methods = true,
-	have_guids = true,
+	object_guids = true,
 }
 
 function core.has_feature(arg)
