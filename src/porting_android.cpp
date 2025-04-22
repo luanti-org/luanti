minetest/minetest

Notify Android player when currently playing inside a world/server
When tapped, the notification will open the currently running game.
The notification is ongoing, i.e. can not be cleared/swiped by the user.
Known issue: The notification still exists after removing Minetest from the recent application list while the player is still inside a world/server. Restarting Minetest (or via tapping the notification) will clear it up.

src/porting_android.cpp
@@ -207,6 +207,18 @@ void shareFileAndroid(const std::string &path)
 	jnienv->CallVoidMethod(app_global->activity->clazz, url_open, jurl);
 }
 
+void setPlayingNowNotification(bool show)
+{
+	jmethodID play_notification = jnienv->GetMethodID(nativeActivity,
+			"setPlayingNowNotification", "(Z)V");
+
+	FATAL_ERROR_IF(play_notification == nullptr,
+			"porting::setPlayingNowNotification unable to find Java setPlayingNowNotification method");
+
+	jboolean jshow = show;
+	jnienv->CallVoidMethod(app_global->activity->clazz, play_notification, jshow);
+}
+
 int getInputDialogState()
 {
 	jmethodID dialogstate = jnienv->GetMethodID(nativeActivity,

luanti-org/luanti

src/porting_android.cpp
@@ -187,6 +187,18 @@ void shareFileAndroid(const std::string &path)
 	jnienv->CallVoidMethod(activity, url_open, jurl);
 }
 
+void setPlayingNowNotification(bool show)
+{
+	jmethodID play_notification = jnienv->GetMethodID(activityClass,
+			"setPlayingNowNotification", "(Z)V");
+
+	FATAL_ERROR_IF(play_notification == nullptr,
+			"porting::setPlayingNowNotification unable to find Java setPlayingNowNotification method");
+
+	jboolean jshow = show;
+	jnienv->CallVoidMethod(activity, play_notification, jshow);
+}
+
 AndroidDialogType getLastInputDialogType()
 {
 	jmethodID lastdialogtype = jnienv->GetMethodID(activityClass,