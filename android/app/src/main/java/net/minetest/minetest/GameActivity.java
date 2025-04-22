66b5adc7ed03f6043ecf673f381c90d362249cf4
grorp <grorp@posteo.de>
2025-04-21 21:27:19 -0400

    Put new API usage behind version check

--- android/app/src/main/java/net/minetest/minetest/GameActivity.java
+++ android/app/src/main/java/net/minetest/minetest/GameActivity.java
@@ -303,27 +303,30 @@ public class GameActivity extends SDLActivity {
 		builder.setContentTitle(getString(R.string.game_notification_title))
 			.setSmallIcon(R.mipmap.ic_launcher)
 			.setContentIntent(intent)
-			.setOngoing(true)
+			.setOngoing(true);
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 			// This avoids a stuck notification if the app is killed while
 			// in-game: (1) if the user closes the app from the "Recents" screen
 			// or (2) if the system kills the app while it is in background.
 			// onStop is called too early to remove the notification and
 			// onDestroy is often not called at all, so there's this hack instead.
-			.setTimeoutAfter(16000);
+			builder.setTimeoutAfter(16000);
+
+			// Replace the notification just before it expires as long as the app is
+			// running (and we're still in-game).
+			final Handler handler = new Handler(Looper.getMainLooper());
+			handler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					if (gameNotificationShown) {
+						updateGameNotification();
+					}
+				}
+			}, 15000);
+		}
 
 		mNotifyManager.notify(MainActivity.NOTIFICATION_ID_GAME, builder.build());
-
-		// Replace the notification just before it expires as long as the app is
-		// still running.
-		final Handler handler = new Handler(Looper.getMainLooper());
-		handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				if (gameNotificationShown) {
-					updateGameNotification();
-				}
-			}
-		}, 15000);
 	}
 
 
