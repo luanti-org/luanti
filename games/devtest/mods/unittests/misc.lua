grorp/minetest

Add Lua binary network serialization functions

games/devtest/mods/unittests/misc.lua
@@ -240,3 +240,202 @@ local function test_gennotify_api()
 	assert(#custom == 0, "custom ids not empty")
 end
 unittests.register("test_gennotify_api", test_gennotify_api)
+
+unittests.register("test_encode_network", function()
+	-- 8-bit integers
+	assert(minetest.encode_network("bbbbbbb", 0, 1, -1, -128, 127, 255, 256) ==
+			"\x00\x01\xFF\x80\x7F\xFF\x00")
+	assert(minetest.encode_network("BBBBBBB", 0, 1, -1, -128, 127, 255, 256) ==
+			"\x00\x01\xFF\x80\x7F\xFF\x00")
+
+	-- 16-bit integers
+	assert(minetest.encode_network("hhhhhhhh",
+			0,          1,          257,        -1,
+			-32768,     32767,      65535,      65536) ==
+			"\x00\x00".."\x00\x01".."\x01\x01".."\xFF\xFF"..
+			"\x80\x00".."\x7F\xFF".."\xFF\xFF".."\x00\x00")
+	assert(minetest.encode_network("HHHHHHHH",
+			0,          1,          257,        -1,
+			-32768,     32767,      65535,      65536) ==
+			"\x00\x00".."\x00\x01".."\x01\x01".."\xFF\xFF"..
+			"\x80\x00".."\x7F\xFF".."\xFF\xFF".."\x00\x00")
+
+	-- 32-bit integers
+	assert(minetest.encode_network("iiiiiiii",
+			0,                  257,                2^24-1,             -1,
+			-2^31,              2^31-1,             2^32-1,             2^32) ==
+			"\x00\x00\x00\x00".."\x00\x00\x01\x01".."\x00\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF"..
+			"\x80\x00\x00\x00".."\x7F\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF".."\x00\x00\x00\x00")
+	assert(minetest.encode_network("IIIIIIII",
+			0,                  257,                2^24-1,             -1,
+			-2^31,              2^31-1,             2^32-1,             2^32) ==
+			"\x00\x00\x00\x00".."\x00\x00\x01\x01".."\x00\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF"..
+			"\x80\x00\x00\x00".."\x7F\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF".."\x00\x00\x00\x00")
+
+	-- 64-bit integers
+	assert(minetest.encode_network("llllll",
+			0,                                  1,
+			511,                                -1,
+			2^53-1,                             -2^53) ==
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")
+	assert(minetest.encode_network("LLLLLL",
+			0,                                  1,
+			511,                                -1,
+			2^53-1,                             -2^53) ==
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")
+
+	-- Strings
+	local max_16 = string.rep("*", 2^16 - 1)
+	local max_32 = string.rep("*", 2^26)
+
+	assert(minetest.encode_network("ssss",
+			"",                 "hello",
+			max_16,             max_16.."too long") ==
+			"\x00\x00"..        "\x00\x05hello"..
+			"\xFF\xFF"..max_16.."\xFF\xFF"..max_16)
+	assert(minetest.encode_network("SSSS",
+			"",                         "hello",
+			max_32,                     max_32.."too long") ==
+			"\x00\x00\x00\x00"..        "\x00\x00\x00\x05hello"..
+			"\x04\x00\x00\x00"..max_32.."\x04\x00\x00\x00"..max_32)
+	assert(minetest.encode_network("zzzz",
+			"",   "hello",   "hello\0embedded", max_16.."longer") ==
+			"\0".."hello\0".."hello\0"..        max_16.."longer\0")
+	assert(minetest.encode_network("ZZZZ",
+			"", "hello", "hello\0embedded", max_16.."longer") ==
+			"".."hello".."hello\0embedded"..max_16.."longer")
+
+	-- Spaces
+	assert(minetest.encode_network("B I", 255, 2^31) == "\xFF\x80\x00\x00\x00")
+	assert(minetest.encode_network("  B Zz ", 15, "abc", "xyz") == "\x0Fabcxyz\0")
+
+	-- Empty format strings
+	assert(minetest.encode_network("") == "")
+	assert(minetest.encode_network("   ", 5, "extra args") == "")
+end)
+
+unittests.register("test_decode_network", function()
+	local d
+
+	-- 8-bit integers
+	d = {minetest.decode_network("bbbbb", "\x00\x01\x7F\x80\xFF")}
+	assert(#d == 5)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 127 and d[4] == -128 and d[5] == -1)
+
+	d = {minetest.decode_network("BBBBB", "\x00\x01\x7F\x80\xFF")}
+	assert(#d == 5)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 127 and d[4] == 128 and d[5] == 255)
+
+	-- 16-bit integers
+	d = {minetest.decode_network("hhhhhh",
+			"\x00\x00".."\x00\x01".."\x01\x01"..
+			"\x7F\xFF".."\x80\x00".."\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 257 and
+			d[4] == 32767 and d[5] == -32768 and d[6] == -1)
+
+	d = {minetest.decode_network("HHHHHH",
+			"\x00\x00".."\x00\x01".."\x01\x01"..
+			"\x7F\xFF".."\x80\x00".."\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 257 and
+			d[4] == 32767 and d[5] == 32768 and d[6] == 65535)
+
+	-- 32-bit integers
+	d = {minetest.decode_network("iiiiii",
+			"\x00\x00\x00\x00".."\x00\x00\x00\x01".."\x00\xFF\xFF\xFF"..
+			"\x7F\xFF\xFF\xFF".."\x80\x00\x00\x00".."\xFF\xFF\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 2^24-1 and
+			d[4] == 2^31-1 and d[5] == -2^31 and d[6] == -1)
+
+	d = {minetest.decode_network("IIIIII",
+			"\x00\x00\x00\x00".."\x00\x00\x00\x01".."\x00\xFF\xFF\xFF"..
+			"\x7F\xFF\xFF\xFF".."\x80\x00\x00\x00".."\xFF\xFF\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 2^24-1 and
+			d[4] == 2^31-1 and d[5] == 2^31 and d[6] == 2^32-1)
+
+	-- 64-bit integers
+	d = {minetest.decode_network("llllll",
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 511 and
+			d[4] == -1 and d[5] == 2^53-1 and d[6] == -2^53)
+
+	d = {minetest.decode_network("LLLLLL",
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 511 and
+			d[4] == 2^64-1 and d[5] == 2^53-1 and d[6] == 2^64 - 2^53)
+
+	-- Floating point numbers
+	local enc = minetest.encode_network("fff",
+			0.0, 123.456, -987.654)
+	assert(#enc == 3 * 4)
+
+	d = {minetest.decode_network("fff", enc)}
+	assert(#d == 3)
+	assert(d[1] == 0.0 and d[2] > 123.45 and d[2] < 123.46 and
+			d[3] > -987.66 and d[3] < -987.65)
+
+	-- Strings
+	local max_16 = string.rep("*", 2^16 - 1)
+	local max_32 = string.rep("*", 2^26)
+
+	d = {minetest.decode_network("ssss",
+			"\x00\x00".."\x00\x05hello".."\xFF\xFF"..max_16.."\x00\xFFtoo short")}
+	assert(#d == 4)
+	assert(d[1] == "" and d[2] == "hello" and d[3] == max_16 and d[4] == "too short")
+
+	d = {minetest.decode_network("SSSSS",
+			"\x00\x00\x00\x00".."\x00\x00\x00\x05hello"..
+			"\x04\x00\x00\x00"..max_32.."\x04\x00\x00\x08"..max_32.."too long"..
+			"\x00\x00\x00\xFFtoo short")}
+	assert(#d == 5)
+	assert(d[1] == "" and d[2] == "hello" and
+			d[3] == max_32 and d[4] == max_32 and d[5] == "too short")
+
+	d = {minetest.decode_network("zzzz", "\0".."hello\0".."missing end")}
+	assert(#d == 4)
+	assert(d[1] == "" and d[2] == "hello" and d[3] == "missing end" and d[4] == "")
+
+	-- Verbatim strings
+	d = {minetest.decode_network("ZZZZ", "xxxyyyyyzzz", 3, 0, 5, -1)}
+	assert(#d == 4)
+	assert(d[1] == "xxx" and d[2] == "" and d[3] == "yyyyy" and d[4] == "zzz")
+
+	-- Read past end
+	d = {minetest.decode_network("bhilBHILf", "")}
+	assert(#d == 9)
+	assert(d[1] == 0 and d[2] == 0 and d[3] == 0 and d[4] == 0 and
+			d[5] == 0 and d[6] == 0 and d[7] == 0 and d[8] == 0 and d[9] == 0.0)
+
+	d = {minetest.decode_network("ZsSzZ", "xx", 4, 4)}
+	assert(#d == 5)
+	assert(d[1] == "xx\0\0" and d[2] == "" and d[3] == "" and
+			d[4] == "" and d[5] == "\0\0\0\0")
+
+	-- Spaces
+	d = {minetest.decode_network("B I", "\xFF\x80\x00\x00\x00")}
+	assert(#d == 2)
+	assert(d[1] == 255 and d[2] == 2^31)
+
+	d = {minetest.decode_network("  B Zz ", "\x0Fabcxyz\0", 3)}
+	assert(#d == 3)
+	assert(d[1] == 15 and d[2] == "abc" and d[3] == "xyz")
+
+	-- Empty format strings
+	d = {minetest.decode_network("", "some random data")}
+	assert(#d == 0)
+	d = {minetest.decode_network("   ", "some random data", 3, 5)}
+	assert(#d == 0)
+end)

src/script/lua_api/l_util.cpp
@@ -583,6 +583,20 @@ int ModApiUtil::l_colorspec_to_colorstring(lua_State *L)
 	return 0;
 }
 
+// colorspec_to_colorint(colorspec)
+int ModApiUtil::l_colorspec_to_colorint(lua_State *L)
+{
+	NO_MAP_LOCK_REQUIRED;
+
+	video::SColor color(0);
+	if (read_color(L, 1, &color)) {
+		lua_pushnumber(L, color.color);
+		return 1;
+	}
+
+	return 0;
+}
+
 // colorspec_to_bytes(colorspec)
 int ModApiUtil::l_colorspec_to_bytes(lua_State *L)
 {
@@ -601,6 +615,183 @@ int ModApiUtil::l_colorspec_to_bytes(lua_State *L)
 	}
 
 	return 0;
+}
+
+// encode_network(format, ...)
+int ModApiUtil::l_encode_network(lua_State *L)
+{
+	NO_MAP_LOCK_REQUIRED;
+
+	std::string format = readParam<std::string>(L, 1);
+	std::ostringstream os(std::ios_base::binary);
+
+	int arg = 2;
+	for (size_t i = 0; i < format.size(); i++) {
+		switch (format[i]) {
+		case 'b':
+			// Casting the double to a signed integer larger than the target
+			// integer results in proper integer wraparound behavior.
+			writeS8(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'h':
+			writeS16(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'i':
+			writeS32(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'l':
+			writeS64(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'B':
+			// Casting to an unsigned integer doesn't result in the proper
+			// integer conversions being applied, so we still use signed.
+			writeU8(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'H':
+			writeU16(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'I':
+			writeU32(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'L':
+			// For the 64-bit integers, we can never experience integer
+			// overflow due to the limited range of Lua's doubles, but we can
+			// have underflow, hence why we cast to s64 first.
+			writeU64(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'f':
+			writeF32(os, luaL_checknumber(L, arg));
+			break;
+		case 's': {
+			std::string str = readParam<std::string>(L, arg);
+			os << serializeString16(str, true);
+			break;
+		}
+		case 'S': {
+			std::string str = readParam<std::string>(L, arg);
+			os << serializeString32(str, true);
+			break;
+		}
+		case 'z': {
+			std::string str = readParam<std::string>(L, arg);
+			os << str.substr(0, strlen(str.c_str())) << '\0';
+			break;
+		}
+		case 'Z':
+			os << readParam<std::string>(L, arg);
+			break;
+		case ' ':
+			// Continue because we don't want to increment arg.
+			continue;
+		default:
+			throw LuaError("Invalid format string");
+		}
+
+		arg++;
+	}
+
+	std::string data = os.str();
+	lua_pushlstring(L, data.c_str(), data.size());
+	return 1;
+}
+
+// decode_network(format, data)
+int ModApiUtil::l_decode_network(lua_State *L)
+{
+	NO_MAP_LOCK_REQUIRED;
+
+	std::string format = readParam<std::string>(L, 1);
+	std::string data = readParam<std::string>(L, 2);
+	std::istringstream is(data, std::ios_base::binary);
+
+	// Make sure we have space for all our returned arguments.
+	lua_checkstack(L, format.size());
+
+	// Set up tracking for verbatim strings and the number of return values.
+	int num_args = lua_gettop(L);
+	int arg = 3;
+	int ret = 0;
+
+	for (size_t i = 0; i < format.size(); i++) {
+		switch (format[i]) {
+		case 'b':
+			lua_pushnumber(L, readS8(is));
+			break;
+		case 'h':
+			lua_pushnumber(L, readS16(is));
+			break;
+		case 'i':
+			lua_pushnumber(L, readS32(is));
+			break;
+		case 'l':
+			lua_pushnumber(L, readS64(is));
+			break;
+		case 'B':
+			lua_pushnumber(L, readU8(is));
+			break;
+		case 'H':
+			lua_pushnumber(L, readU16(is));
+			break;
+		case 'I':
+			lua_pushnumber(L, readU32(is));
+			break;
+		case 'L':
+			lua_pushnumber(L, readU64(is));
+			break;
+		case 'f':
+			lua_pushnumber(L, readF32(is));
+			break;
+		case 's': {
+			std::string str = deSerializeString16(is, true);
+			lua_pushlstring(L, str.c_str(), str.size());
+			break;
+		}
+		case 'S': {
+			std::string str = deSerializeString32(is, true);
+			lua_pushlstring(L, str.c_str(), str.size());
+			break;
+		}
+		case 'z': {
+			std::string str;
+			std::getline(is, str, '\0');
+
+			lua_pushlstring(L, str.c_str(), str.size());
+			break;
+		}
+		case 'Z': {
+			if (arg > num_args) {
+				throw LuaError("Missing verbatim string size");
+			}
+
+			double size = luaL_checknumber(L, arg);
+			std::string str;
+
+			if (size < 0) {
+				// Read the entire rest of the input stream.
+				std::ostringstream os(std::ios_base::binary);
+				os << is.rdbuf();
+				str = os.str();
+			} else if (size != 0) {
+				// Read the specified number of characters.
+				str.resize(size);
+				is.read(&str[0], size);
+			}
+
+			lua_pushlstring(L, str.c_str(), str.size());
+			arg++;
+			break;
+		}
+		case ' ':
+			// Continue because we don't want to increment ret.
+			continue;
+		default:
+			throw LuaError("Invalid format string");
+		}
+
+		ret++;
+	}
+
+	return ret;
 }
 
 // encode_png(w, h, data, level)
@@ -691,7 +882,11 @@ void ModApiUtil::Initialize(lua_State *L, int top)
 	API_FCT(get_version);
 	API_FCT(sha1);
 	API_FCT(colorspec_to_colorstring);
+	API_FCT(colorspec_to_colorint);
 	API_FCT(colorspec_to_bytes);
+
+	API_FCT(encode_network);
+	API_FCT(decode_network);
 
 	API_FCT(encode_png);
 
@@ -724,10 +919,14 @@ void ModApiUtil::InitializeClient(lua_State *L, int top)
 	API_FCT(get_version);
 	API_FCT(sha1);
 	API_FCT(colorspec_to_colorstring);
+	API_FCT(colorspec_to_colorint);
 	API_FCT(colorspec_to_bytes);
 
 	API_FCT(get_last_run_mod);
 	API_FCT(set_last_run_mod);
+
+	API_FCT(encode_network);
+	API_FCT(decode_network);
 
 	API_FCT(urlencode);
 
@@ -767,7 +966,11 @@ void ModApiUtil::InitializeAsync(lua_State *L, int top)
 	API_FCT(get_version);
 	API_FCT(sha1);
 	API_FCT(colorspec_to_colorstring);
+	API_FCT(colorspec_to_colorint);
 	API_FCT(colorspec_to_bytes);
+
+	API_FCT(encode_network);
+	API_FCT(decode_network);
 
 	API_FCT(encode_png);
 

src/script/lua_api/l_util.h
@@ -20,6 +20,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #pragma once
 
 #include "lua_api/l_base.h"
+#include "util/serialize.h"
 
 class AsyncEngine;
 
@@ -116,8 +117,17 @@ private:
 	// colorspec_to_colorstring(colorspec)
 	static int l_colorspec_to_colorstring(lua_State *L);
 
+	// colorspec_to_colorint(colorspec)
+	static int l_colorspec_to_colorint(lua_State *L);
+
 	// colorspec_to_bytes(colorspec)
 	static int l_colorspec_to_bytes(lua_State *L);
+
+	// encode_network(format, ...)
+	static int l_encode_network(lua_State *L);
+
+	// decode_network(format, data)
+	static int l_decode_network(lua_State *L);
 
 	// encode_png(w, h, data, level)
 	static int l_encode_png(lua_State *L);

luanti-org/luanti

games/devtest/mods/unittests/misc.lua
@@ -353,3 +353,202 @@ local function test_ipc_poll(cb)
 	print("delta: " .. (core.get_us_time() - t0) .. "us")
 end
 unittests.register("test_ipc_poll", test_ipc_poll)
+
+unittests.register("test_encode_network", function()
+	-- 8-bit integers
+	assert(core.encode_network("bbbbbbb", 0, 1, -1, -128, 127, 255, 256) ==
+			"\x00\x01\xFF\x80\x7F\xFF\x00")
+	assert(core.encode_network("BBBBBBB", 0, 1, -1, -128, 127, 255, 256) ==
+			"\x00\x01\xFF\x80\x7F\xFF\x00")
+
+	-- 16-bit integers
+	assert(core.encode_network("hhhhhhhh",
+			0,          1,          257,        -1,
+			-32768,     32767,      65535,      65536) ==
+			"\x00\x00".."\x00\x01".."\x01\x01".."\xFF\xFF"..
+			"\x80\x00".."\x7F\xFF".."\xFF\xFF".."\x00\x00")
+	assert(core.encode_network("HHHHHHHH",
+			0,          1,          257,        -1,
+			-32768,     32767,      65535,      65536) ==
+			"\x00\x00".."\x00\x01".."\x01\x01".."\xFF\xFF"..
+			"\x80\x00".."\x7F\xFF".."\xFF\xFF".."\x00\x00")
+
+	-- 32-bit integers
+	assert(core.encode_network("iiiiiiii",
+			0,                  257,                2^24-1,             -1,
+			-2^31,              2^31-1,             2^32-1,             2^32) ==
+			"\x00\x00\x00\x00".."\x00\x00\x01\x01".."\x00\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF"..
+			"\x80\x00\x00\x00".."\x7F\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF".."\x00\x00\x00\x00")
+	assert(core.encode_network("IIIIIIII",
+			0,                  257,                2^24-1,             -1,
+			-2^31,              2^31-1,             2^32-1,             2^32) ==
+			"\x00\x00\x00\x00".."\x00\x00\x01\x01".."\x00\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF"..
+			"\x80\x00\x00\x00".."\x7F\xFF\xFF\xFF".."\xFF\xFF\xFF\xFF".."\x00\x00\x00\x00")
+
+	-- 64-bit integers
+	assert(core.encode_network("llllll",
+			0,                                  1,
+			511,                                -1,
+			2^53-1,                             -2^53) ==
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")
+	assert(core.encode_network("LLLLLL",
+			0,                                  1,
+			511,                                -1,
+			2^53-1,                             -2^53) ==
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")
+
+	-- Strings
+	local max_16 = string.rep("*", 2^16 - 1)
+	local max_32 = string.rep("*", 2^26)
+
+	assert(core.encode_network("ssss",
+			"",                 "hello",
+			max_16,             max_16.."too long") ==
+			"\x00\x00"..        "\x00\x05hello"..
+			"\xFF\xFF"..max_16.."\xFF\xFF"..max_16)
+	assert(core.encode_network("SSSS",
+			"",                         "hello",
+			max_32,                     max_32.."too long") ==
+			"\x00\x00\x00\x00"..        "\x00\x00\x00\x05hello"..
+			"\x04\x00\x00\x00"..max_32.."\x04\x00\x00\x00"..max_32)
+	assert(core.encode_network("zzzz",
+			"",   "hello",   "hello\0embedded", max_16.."longer") ==
+			"\0".."hello\0".."hello\0"..        max_16.."longer\0")
+	assert(core.encode_network("ZZZZ",
+			"", "hello", "hello\0embedded", max_16.."longer") ==
+			"".."hello".."hello\0embedded"..max_16.."longer")
+
+	-- Spaces
+	assert(core.encode_network("B I", 255, 2^31) == "\xFF\x80\x00\x00\x00")
+	assert(core.encode_network("  B Zz ", 15, "abc", "xyz") == "\x0Fabcxyz\0")
+
+	-- Empty format strings
+	assert(core.encode_network("") == "")
+	assert(core.encode_network("   ", 5, "extra args") == "")
+end)
+
+unittests.register("test_decode_network", function()
+	local d
+
+	-- 8-bit integers
+	d = {core.decode_network("bbbbb", "\x00\x01\x7F\x80\xFF")}
+	assert(#d == 5)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 127 and d[4] == -128 and d[5] == -1)
+
+	d = {core.decode_network("BBBBB", "\x00\x01\x7F\x80\xFF")}
+	assert(#d == 5)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 127 and d[4] == 128 and d[5] == 255)
+
+	-- 16-bit integers
+	d = {core.decode_network("hhhhhh",
+			"\x00\x00".."\x00\x01".."\x01\x01"..
+			"\x7F\xFF".."\x80\x00".."\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 257 and
+			d[4] == 32767 and d[5] == -32768 and d[6] == -1)
+
+	d = {core.decode_network("HHHHHH",
+			"\x00\x00".."\x00\x01".."\x01\x01"..
+			"\x7F\xFF".."\x80\x00".."\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 257 and
+			d[4] == 32767 and d[5] == 32768 and d[6] == 65535)
+
+	-- 32-bit integers
+	d = {core.decode_network("iiiiii",
+			"\x00\x00\x00\x00".."\x00\x00\x00\x01".."\x00\xFF\xFF\xFF"..
+			"\x7F\xFF\xFF\xFF".."\x80\x00\x00\x00".."\xFF\xFF\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 2^24-1 and
+			d[4] == 2^31-1 and d[5] == -2^31 and d[6] == -1)
+
+	d = {core.decode_network("IIIIII",
+			"\x00\x00\x00\x00".."\x00\x00\x00\x01".."\x00\xFF\xFF\xFF"..
+			"\x7F\xFF\xFF\xFF".."\x80\x00\x00\x00".."\xFF\xFF\xFF\xFF")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 2^24-1 and
+			d[4] == 2^31-1 and d[5] == 2^31 and d[6] == 2^32-1)
+
+	-- 64-bit integers
+	d = {core.decode_network("llllll",
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 511 and
+			d[4] == -1 and d[5] == 2^53-1 and d[6] == -2^53)
+
+	d = {core.decode_network("LLLLLL",
+			"\x00\x00\x00\x00\x00\x00\x00\x00".."\x00\x00\x00\x00\x00\x00\x00\x01"..
+			"\x00\x00\x00\x00\x00\x00\x01\xFF".."\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"..
+			"\x00\x1F\xFF\xFF\xFF\xFF\xFF\xFF".."\xFF\xE0\x00\x00\x00\x00\x00\x00")}
+	assert(#d == 6)
+	assert(d[1] == 0 and d[2] == 1 and d[3] == 511 and
+			d[4] == 2^64-1 and d[5] == 2^53-1 and d[6] == 2^64 - 2^53)
+
+	-- Floating point numbers
+	local enc = core.encode_network("fff",
+			0.0, 123.456, -987.654)
+	assert(#enc == 3 * 4)
+
+	d = {core.decode_network("fff", enc)}
+	assert(#d == 3)
+	assert(d[1] == 0.0 and d[2] > 123.45 and d[2] < 123.46 and
+			d[3] > -987.66 and d[3] < -987.65)
+
+	-- Strings
+	local max_16 = string.rep("*", 2^16 - 1)
+	local max_32 = string.rep("*", 2^26)
+
+	d = {core.decode_network("ssss",
+			"\x00\x00".."\x00\x05hello".."\xFF\xFF"..max_16.."\x00\xFFtoo short")}
+	assert(#d == 4)
+	assert(d[1] == "" and d[2] == "hello" and d[3] == max_16 and d[4] == "too short")
+
+	d = {core.decode_network("SSSSS",
+			"\x00\x00\x00\x00".."\x00\x00\x00\x05hello"..
+			"\x04\x00\x00\x00"..max_32.."\x04\x00\x00\x08"..max_32.."too long"..
+			"\x00\x00\x00\xFFtoo short")}
+	assert(#d == 5)
+	assert(d[1] == "" and d[2] == "hello" and
+			d[3] == max_32 and d[4] == max_32 and d[5] == "too short")
+
+	d = {core.decode_network("zzzz", "\0".."hello\0".."missing end")}
+	assert(#d == 4)
+	assert(d[1] == "" and d[2] == "hello" and d[3] == "missing end" and d[4] == "")
+
+	-- Verbatim strings
+	d = {core.decode_network("ZZZZ", "xxxyyyyyzzz", 3, 0, 5, -1)}
+	assert(#d == 4)
+	assert(d[1] == "xxx" and d[2] == "" and d[3] == "yyyyy" and d[4] == "zzz")
+
+	-- Read past end
+	d = {core.decode_network("bhilBHILf", "")}
+	assert(#d == 9)
+	assert(d[1] == 0 and d[2] == 0 and d[3] == 0 and d[4] == 0 and
+			d[5] == 0 and d[6] == 0 and d[7] == 0 and d[8] == 0 and d[9] == 0.0)
+
+	d = {core.decode_network("ZsSzZ", "xx", 4, 4)}
+	assert(#d == 5)
+	assert(d[1] == "xx\0\0" and d[2] == "" and d[3] == "" and
+			d[4] == "" and d[5] == "\0\0\0\0")
+
+	-- Spaces
+	d = {core.decode_network("B I", "\xFF\x80\x00\x00\x00")}
+	assert(#d == 2)
+	assert(d[1] == 255 and d[2] == 2^31)
+
+	d = {core.decode_network("  B Zz ", "\x0Fabcxyz\0", 3)}
+	assert(#d == 3)
+	assert(d[1] == 15 and d[2] == "abc" and d[3] == "xyz")
+
+	-- Empty format strings
+	d = {core.decode_network("", "some random data")}
+	assert(#d == 0)
+	d = {core.decode_network("   ", "some random data", 3, 5)}
+	assert(#d == 0)
+end)

src/script/lua_api/l_util.cpp
@@ -625,6 +625,197 @@ int ModApiUtil::l_colorspec_to_table(lua_State *L)
 	return 0;
 }
 
+// colorspec_to_int(colorspec)
+int ModApiUtil::l_colorspec_to_int(lua_State *L)
+{
+	NO_MAP_LOCK_REQUIRED;
+
+	video::SColor color(0);
+	if (read_color(L, 1, &color)) {
+		lua_pushnumber(L, color.color);
+		return 1;
+	}
+
+	return 0;
+}
+
+// encode_network(format, ...)
+int ModApiUtil::l_encode_network(lua_State *L)
+{
+	NO_MAP_LOCK_REQUIRED;
+
+	std::string format = readParam<std::string>(L, 1);
+	std::ostringstream os(std::ios_base::binary);
+
+	int arg = 2;
+	for (size_t i = 0; i < format.size(); i++) {
+		switch (format[i]) {
+		case 'b':
+			// Casting the double to a signed integer larger than the target
+			// integer results in proper integer wraparound behavior.
+			writeS8(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'h':
+			writeS16(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'i':
+			writeS32(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'l':
+			writeS64(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'B':
+			// Casting to an unsigned integer doesn't result in the proper
+			// integer conversions being applied, so we still use signed.
+			writeU8(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'H':
+			writeU16(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'I':
+			writeU32(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'L':
+			// For the 64-bit integers, we can never experience integer
+			// overflow due to the limited range of Lua's doubles, but we can
+			// have underflow, hence why we cast to s64 first.
+			writeU64(os, (s64)luaL_checknumber(L, arg));
+			break;
+		case 'f':
+			writeF32(os, luaL_checknumber(L, arg));
+			break;
+		case 's': {
+			std::string str = readParam<std::string>(L, arg);
+			os << serializeString16(str, true);
+			break;
+		}
+		case 'S': {
+			std::string str = readParam<std::string>(L, arg);
+			os << serializeString32(str, true);
+			break;
+		}
+		case 'z': {
+			std::string str = readParam<std::string>(L, arg);
+			os << std::string_view(str.c_str(), strlen(str.c_str())) << '\0';
+			break;
+		}
+		case 'Z':
+			os << readParam<std::string>(L, arg);
+			break;
+		case ' ':
+			// Continue because we don't want to increment arg.
+			continue;
+		default:
+			throw LuaError("Invalid format string");
+		}
+
+		arg++;
+	}
+
+	std::string data = os.str();
+	lua_pushlstring(L, data.c_str(), data.size());
+	return 1;
+}
+
+// decode_network(format, data)
+int ModApiUtil::l_decode_network(lua_State *L)
+{
+	NO_MAP_LOCK_REQUIRED;
+
+	std::string format = readParam<std::string>(L, 1);
+	std::string data = readParam<std::string>(L, 2);
+	std::istringstream is(data, std::ios_base::binary);
+
+	// Make sure we have space for all our returned arguments.
+	lua_checkstack(L, format.size());
+
+	// Set up tracking for verbatim strings and the number of return values.
+	int num_args = lua_gettop(L);
+	int arg = 3;
+	int ret = 0;
+
+	for (size_t i = 0; i < format.size(); i++) {
+		switch (format[i]) {
+		case 'b':
+			lua_pushnumber(L, readS8(is));
+			break;
+		case 'h':
+			lua_pushnumber(L, readS16(is));
+			break;
+		case 'i':
+			lua_pushnumber(L, readS32(is));
+			break;
+		case 'l':
+			lua_pushnumber(L, readS64(is));
+			break;
+		case 'B':
+			lua_pushnumber(L, readU8(is));
+			break;
+		case 'H':
+			lua_pushnumber(L, readU16(is));
+			break;
+		case 'I':
+			lua_pushnumber(L, readU32(is));
+			break;
+		case 'L':
+			lua_pushnumber(L, readU64(is));
+			break;
+		case 'f':
+			lua_pushnumber(L, readF32(is));
+			break;
+		case 's': {
+			std::string str = deSerializeString16(is, true);
+			lua_pushlstring(L, str.c_str(), str.size());
+			break;
+		}
+		case 'S': {
+			std::string str = deSerializeString32(is, true);
+			lua_pushlstring(L, str.c_str(), str.size());
+			break;
+		}
+		case 'z': {
+			std::string str;
+			std::getline(is, str, '\0');
+
+			lua_pushlstring(L, str.c_str(), str.size());
+			break;
+		}
+		case 'Z': {
+			if (arg > num_args) {
+				throw LuaError("Missing verbatim string size");
+			}
+
+			double size = luaL_checknumber(L, arg);
+			std::string str;
+
+			if (size < 0) {
+				// Read the entire rest of the input stream.
+				std::ostringstream os(std::ios_base::binary);
+				os << is.rdbuf();
+				str = os.str();
+			} else if (size != 0) {
+				// Read the specified number of characters.
+				str.resize(size);
+				is.read(&str[0], size);
+			}
+
+			lua_pushlstring(L, str.c_str(), str.size());
+			arg++;
+			break;
+		}
+		case ' ':
+			// Continue because we don't want to increment ret.
+			continue;
+		default:
+			throw LuaError("Invalid format string");
+		}
+
+		ret++;
+	}
+
+	return ret;
+}
+
 // time_to_day_night_ratio(time_of_day)
 int ModApiUtil::l_time_to_day_night_ratio(lua_State *L)
 {
@@ -737,8 +928,12 @@ void ModApiUtil::Initialize(lua_State *L, int top)
 	API_FCT(colorspec_to_colorstring);
 	API_FCT(colorspec_to_bytes);
 	API_FCT(colorspec_to_table);
-	API_FCT(time_to_day_night_ratio);
+	API_FCT(colorspec_to_int);
 
+	API_FCT(encode_network);
+	API_FCT(decode_network);
+
+	API_FCT(time_to_day_night_ratio);
 	API_FCT(encode_png);
 
 	API_FCT(get_last_run_mod);
@@ -774,6 +969,11 @@ void ModApiUtil::InitializeClient(lua_State *L, int top)
 	API_FCT(colorspec_to_colorstring);
 	API_FCT(colorspec_to_bytes);
 	API_FCT(colorspec_to_table);
+	API_FCT(colorspec_to_int);
+
+	API_FCT(encode_network);
+	API_FCT(decode_network);
+
 	API_FCT(time_to_day_night_ratio);
 
 	API_FCT(get_last_run_mod);
@@ -820,8 +1020,12 @@ void ModApiUtil::InitializeAsync(lua_State *L, int top)
 	API_FCT(colorspec_to_colorstring);
 	API_FCT(colorspec_to_bytes);
 	API_FCT(colorspec_to_table);
-	API_FCT(time_to_day_night_ratio);
+	API_FCT(colorspec_to_int);
 
+	API_FCT(encode_network);
+	API_FCT(decode_network);
+
+	API_FCT(time_to_day_night_ratio);
 	API_FCT(encode_png);
 
 	API_FCT(get_last_run_mod);

src/script/lua_api/l_util.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "lua_api/l_base.h"
+#include "util/serialize.h"
 
 class AsyncEngine;
 
@@ -109,6 +110,15 @@ private:
 
 	// colorspec_to_table(colorspec)
 	static int l_colorspec_to_table(lua_State *L);
+
+	// colorspec_to_int(colorspec)
+	static int l_colorspec_to_int(lua_State *L);
+
+	// encode_network(format, ...)
+	static int l_encode_network(lua_State *L);
+
+	// decode_network(format, data)
+	static int l_decode_network(lua_State *L);
 
 	// time_to_day_night_ratio(time_of_day)
 	static int l_time_to_day_night_ratio(lua_State *L);