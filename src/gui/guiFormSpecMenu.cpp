a9844bef5b6e537fe117c7f791ea49c3501a2b3c
siliconsniffer <97843108+siliconsniffer@users.noreply.github.com>
2025-05-30 11:22:55 +0200

    Less indents.

--- src/gui/guiFormSpecMenu.cpp
+++ src/gui/guiFormSpecMenu.cpp
@@ -4068,22 +4068,25 @@ bool GUIFormSpecMenu::OnEvent(const SEvent& event)
 		if (event.KeyInput.PressedDown && (event.KeyInput.Key == KEY_TAB && ctrl)) {
 			// Try to find a tab control among our elements
 			for (const FieldSpec &s : m_fields) {
-				if (s.ftype == f_TabHeader) {
-					IGUIElement *element = getElementFromId(s.fid, true);
-					if (element && element->getType() == gui::EGUIET_TAB_CONTROL) {
-						gui::IGUITabControl *tabs = static_cast<gui::IGUITabControl *>(element);
-						s32 num_tabs = tabs->getTabCount();
-						s32 active = tabs->getActiveTab();
-						if (num_tabs > 1) {
-							// Shift: Previous tab, No shift: Next tab
-							active = (active + (shift ? -1 : 1) + num_tabs) % num_tabs;
-							tabs->setActiveTab(active);
-							// Simulate a tab change event if necessary
-							// Optionally: trigger formspec callbacks if needed
-							return true; // handled
-						}
-					}
-				}
+				if (s.ftype != f_TabHeader)
+					continue;
+
+				IGUIElement *element = getElementFromId(s.fid, true);
+				if (!element || element->getType() != gui::EGUIET_TAB_CONTROL)
+					continue;
+
+				gui::IGUITabControl *tabs = static_cast<gui::IGUITabControl *>(element);
+				s32 num_tabs = tabs->getTabCount();
+				if (num_tabs <= 1)
+					continue;
+
+				s32 active = tabs->getActiveTab();
+				// Shift: Previous tab, No shift: Next tab
+				active = (active + (shift ? -1 : 1) + num_tabs) % num_tabs;
+				tabs->setActiveTab(active);
+				// Simulate a tab change event if necessary
+				// Optionally: trigger formspec callbacks if needed
+				return true; // handled
 			}
 		}
 		if (event.KeyInput.PressedDown && (
