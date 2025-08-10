84605917bbf5e451a75095e36146bad079bf48a4
TheEt1234 <TheEt1234@users.noreply.github.com>
2025-08-10 20:21:47 +0200

    Close #16373 by making the error slightly less confusing

--- builtin/common/misc_helpers.lua
+++ builtin/common/misc_helpers.lua
@@ -119,6 +119,7 @@ end
 -- }]]
 function dump(value, indent)
 	indent = indent or "\t"
+	assert(type(indent) == "string", "dump()'s second argument should be a string or nil, to dump multiple things use dump({a, b}).")
 	local newline = indent == "" and "" or "\n"
 
 	local rope = {}
