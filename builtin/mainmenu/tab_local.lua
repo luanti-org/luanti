dd9e472260cc1ed4160626c3387d56f5095fd46d
birdlover32767 <162155761+birdlover32767@users.noreply.github.com>
2025-08-13 14:44:57 +0300

    Remove whitespace

--- builtin/mainmenu/tab_local.lua
+++ builtin/mainmenu/tab_local.lua
@@ -168,7 +168,7 @@ local function get_formspec(tabview, name, tabdata)
 	local index = filterlist.get_current_index(menudata.worldlist,
 				tonumber(core.settings:get("mainmenu_last_selected_world")))
 	local is_world_selected = index and index ~= 0
-	
+
 	local list = menudata.worldlist:get_list()
 	local world = list and index and list[index]
 	local game
