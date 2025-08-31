b0e691c3c603ca5b3bcd4d9cdf66973e5adba9a1
Lars Mueller <appgurulars@gmx.de>
2025-08-31 18:42:24 +0200

    Acquire envlock

--- src/server.h
+++ src/server.h
@@ -605,7 +605,8 @@ private:
 	void SendDeleteParticleSpawner(session_t peer_id, u32 id);
 
 	// Spawn particles for a specific client.
-	void SendSpawnParticles(session_t peer_id, const std::vector<ParticleParameters> &particles);
+	void SendSpawnParticles(RemotePlayer *player,
+			const std::vector<ParticleParameters> &particles);
 	// Spawn all particles for this step, batching them if clients support it.
 	void SendSpawnParticles();
 
