e4c89c65dd88b1ebd85c0ad1ea510f95791ddea7
cx384 <cx384@proton.me>
2025-10-08 17:21:10 +0200

    Remove meta animation

--- src/tileanimation.h
+++ src/tileanimation.h
@@ -6,7 +6,6 @@
 
 #include <iostream>
 #include "irrlichttypes_bloated.h"
-#include <optional>
 
 enum TileAnimationType : u8
 {
@@ -38,8 +37,6 @@ struct TileAnimationParams
 
 	void serialize(std::ostream &os, u16 protocol_ver) const;
 	void deSerialize(std::istream &is, u16 protocol_ver);
-	void serializeJson(std::ostream &os) const;
-	static std::optional<TileAnimationParams> deserializeJson(std::istream &is);
 
 	void determineParams(v2u32 texture_size, int *frame_count, int *frame_length_ms,
 			v2u32 *frame_size) const;
