8e3981c0dbe2c620a35b66987da07271db22442f
Desour <ds.desour@proton.me>
2025-10-11 12:06:44 +0200

    use relative paths in GenerateBuiltinFilesList.cmake
    also makes GenerateBuiltinFilesList.cmake simpler

--- cmake/Modules/GenerateBuiltinFilesList.cmake
+++ cmake/Modules/GenerateBuiltinFilesList.cmake
@@ -1,18 +1,12 @@
 # Compute sha256 digests of builtin files
 
-string(LENGTH "${BUILTIN_BASE_PATH}" BUILTIN_BASE_PATH_LEN)
-
 set(BUILTIN_SHA256S "")
-foreach(P ${BUILTIN_SRCS})
-	file(SHA256 ${P} H)
+foreach(P_REL ${BUILTIN_SRCS})
+	set(P_ABS "${BUILTIN_BASE_PATH}${P_REL}")
 
-	string(SUBSTRING "${P}" 0 ${BUILTIN_BASE_PATH_LEN} BP)
-	if(NOT (BP STREQUAL BUILTIN_BASE_PATH))
-		message(FATAL_ERROR "Expected ${P} to be a subpath of ${BUILTIN_BASE_PATH}")
-	endif()
-	string(SUBSTRING "${P}" ${BUILTIN_BASE_PATH_LEN} -1 RP)
+	file(SHA256 ${P_ABS} H)
 
-	list(APPEND BUILTIN_SHA256S "{\"${RP}\", \"${H}\"}")
+	list(APPEND BUILTIN_SHA256S "{\"${P_REL}\", \"${H}\"}")
 endforeach()
 
 list(JOIN BUILTIN_SHA256S ",\n" BUILTIN_SHA256S_INITIALIZER_LIST)
