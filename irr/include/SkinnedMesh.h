b6b896fd160e3a9f4af7a9882ef07ccaa6371e71
Lars Mueller <appgurulars@gmx.de>
2025-11-16 03:15:30 +0100

    Remove IAnimatedMesh::[gs]etAnimationSpeed
    This is dead code: We always end up throwing the animation speed away, forcing modders to supply it.
    It is also irrelevant for the future (glTF).

--- irr/include/SkinnedMesh.h
+++ irr/include/SkinnedMesh.h
@@ -59,15 +59,6 @@ public:
 	//! If the duration is 0, it is a static (=non animated) mesh.
 	f32 getMaxFrameNumber() const override;
 
-	//! Gets the default animation speed of the animated mesh.
-	/** \return Amount of frames per second. If the amount is 0, it is a static, non animated mesh. */
-	f32 getAnimationSpeed() const override;
-
-	//! Gets the frame count of the animated mesh.
-	/** \param fps Frames per second to play the animation with. If the amount is 0, it is not animated.
-	The actual speed is set in the scene node the mesh is instantiated in.*/
-	void setAnimationSpeed(f32 fps) override;
-
 	//! Turns the given array of local matrices into an array of global matrices
 	//! by multiplying with respective parent matrices.
 	void calculateGlobalMatrices(std::vector<core::matrix4> &matrices) const;
