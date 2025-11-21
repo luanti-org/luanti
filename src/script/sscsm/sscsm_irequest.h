ccaf92e2a979ff856343fd5814cf6b29511e409c
Desour <ds.desour@proton.me>
2025-11-21 13:48:19 +0100

    add comments in sscsm_irequest.h

--- src/script/sscsm/sscsm_irequest.h
+++ src/script/sscsm/sscsm_irequest.h
@@ -11,12 +11,15 @@
 class SSCSMController;
 class Client;
 
+// FIXME: remove once we have actual serialization
+// this is just here so we can instead put them into unique_ptr
 struct ISSCSMAnswer
 {
 	virtual ~ISSCSMAnswer() = default;
 };
 
-// FIXME: actually serialize, and replace this with a string
+// FIXME: actually serialize, and replace this with a std::vector<u8>
+// (not polymorphic. the receiving side will know the answer type that is in here)
 using SerializedSSCSMAnswer = std::unique_ptr<ISSCSMAnswer>;
 
 // Request made by the sscsm env to the main env.
@@ -27,13 +30,16 @@ struct ISSCSMRequest
 	virtual SerializedSSCSMAnswer exec(Client *client) = 0;
 };
 
-// FIXME: actually serialize, and replace this with a string
+// FIXME: actually serialize, and replace this with a std::vector<u8>
+// (polymorphic. this can be any ISSCSMRequest. ==> needs type tag)
 using SerializedSSCSMRequest = std::unique_ptr<ISSCSMRequest>;
 
 template <typename T>
 inline SerializedSSCSMRequest serializeSSCSMRequest(const T &request)
 {
 	static_assert(std::is_base_of_v<ISSCSMRequest, T>);
+
+	// FIXME: this will need to use a type tag for T
 
 	return std::make_unique<T>(request);
 }
@@ -42,6 +48,10 @@ template <typename T>
 inline T deserializeSSCSMAnswer(SerializedSSCSMAnswer answer_serialized)
 {
 	static_assert(std::is_base_of_v<ISSCSMAnswer, T>);
+
+	// FIXME: should look something like this:
+	// return sscsm::Serializer<T>{}.deserialize(answer_serialized);
+	// (note: answer_serialized does not need a type tag)
 
 	// dynamic cast in place of actual deserialization
 	auto ptr = dynamic_cast<T *>(answer_serialized.get());
@@ -56,12 +66,15 @@ inline SerializedSSCSMAnswer serializeSSCSMAnswer(T &&answer)
 {
 	static_assert(std::is_base_of_v<ISSCSMAnswer, T>);
 
+	// FIXME: should look something like this:
+	// return sscsm::Serializer<T>{}.serialize(request);
+
 	return std::make_unique<T>(std::move(answer));
 }
 
 inline std::unique_ptr<ISSCSMRequest> deserializeSSCSMRequest(SerializedSSCSMRequest request_serialized)
 {
-	// The actual deserialization will have to use a type tag, and then choose
-	// the appropriate deserializer.
+	// FIXME: The actual deserialization will have to use a type tag, and then
+	// choose the appropriate deserializer.
 	return request_serialized;
 }
