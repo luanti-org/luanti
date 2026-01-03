9b35a0cb2b02cc73d2f6295ca2aa2bdb0208f426
veprogames <vepro8673@mailbox.org>
2026-01-03 18:34:57 +0100

    Remove magic constant and adjust setting values

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -567,10 +567,7 @@ void Game::run()
 			cam_view.camera_yaw = cam_view_target.camera_yaw;
 			cam_view.camera_pitch = cam_view_target.camera_pitch;
 		} else {
-			// increase convergence time by an arbitrary value
-			// because input fields have a step of 0.01, which is not fine grained enough
-			const f32 LAMBDA_MULTIPLIER = 10.0f;
-			f32 cam_damp_lambda = LAMBDA_MULTIPLIER / m_cache_cam_smoothing * dtime;
+			f32 cam_damp_lambda = 1.0f / m_cache_cam_smoothing * dtime;
 			cam_view.camera_yaw = damp(
 					cam_view.camera_yaw,
 					cam_view_target.camera_yaw,
