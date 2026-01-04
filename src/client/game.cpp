8abddf882a19eadbe750ebf09fc384676af07419
Zughy <63455151+Zughy@users.noreply.github.com>
2026-01-04 14:50:24 +0100

    Run place/secondary_use functions also when dig is held down

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -2749,6 +2749,13 @@ void Game::processPlayerInteraction(f32 dtime, bool show_hud)
 		if (wasKeyPressed(KeyType::DIG) && (!client->modsLoaded() ||
 				!client->getScript()->on_item_use(selected_item, pointed)))
 			client->interact(INTERACT_USE, pointed);
+
+		if (wasKeyPressed(KeyType::PLACE)) {
+			if (pointed.type == POINTEDTHING_NODE)
+				handlePointingAtNode(pointed, selected_item, hand_item, dtime);
+			else
+				handlePointingAtNothing(selected_item);
+		}
 	} else if (pointed.type == POINTEDTHING_NODE) {
 		handlePointingAtNode(pointed, selected_item, hand_item, dtime);
 	} else if (pointed.type == POINTEDTHING_OBJECT) {
@@ -2762,6 +2769,13 @@ void Game::processPlayerInteraction(f32 dtime, bool show_hud)
 		// Run callback even though item is not usable
 		if (wasKeyPressed(KeyType::DIG) && client->modsLoaded())
 			client->getScript()->on_item_use(selected_item, pointed);
+
+		if (wasKeyPressed(KeyType::PLACE)) {
+			if (pointed.type == POINTEDTHING_NODE)
+				handlePointingAtNode(pointed, selected_item, hand_item, dtime);
+			else
+				handlePointingAtNothing(selected_item);
+		}
 	} else if (wasKeyPressed(KeyType::PLACE)) {
 		handlePointingAtNothing(selected_item);
 	}
