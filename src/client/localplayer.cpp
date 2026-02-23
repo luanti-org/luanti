41458e53a63510acb8c291a5b3c3822e7a552829
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-02-22 22:18:33 -0500

    Fix trailing white space in localplayer

--- src/client/localplayer.cpp
+++ src/client/localplayer.cpp
@@ -328,7 +328,7 @@ void LocalPlayer::move(f32 dtime, Environment *env,
 	const v3f initial_speed = m_speed;
 
 
-	const bool use_new_stair = m_cao != NULL && 
+	const bool use_new_stair = m_cao != NULL &&
 		m_cao->getProperties().new_stair;
 
 	collisionMoveResult result = collisionMoveSimple(env, m_client,
@@ -952,7 +952,7 @@ void LocalPlayer::old_move(f32 dtime, Environment *env,
 	const v3f initial_position = position;
 	const v3f initial_speed = m_speed;
 
-	const bool use_new_stair = m_cao != NULL && 
+	const bool use_new_stair = m_cao != NULL &&
 		m_cao->getProperties().new_stair;
 
 	collisionMoveResult result = collisionMoveSimple(env, m_client,
@@ -1218,12 +1218,12 @@ void LocalPlayer::handleAutojump(f32 dtime, Environment *env,
 	v3f jump_pos = initial_position + v3f(0.0f, jump_height, 0.0f);
 	v3f jump_speed = initial_speed;
 
-	const bool use_new_stair = m_cao != NULL && 
+	const bool use_new_stair = m_cao != NULL &&
 		m_cao->getProperties().new_stair;
 
 	// try at peak of jump, zero step height
 	collisionMoveResult jump_result = collisionMoveSimple(env, m_client,
-		m_collisionbox, 0.0f, dtime, &jump_pos, &jump_speed, v3f(0.0f), m_cao, true, 
+		m_collisionbox, 0.0f, dtime, &jump_pos, &jump_speed, v3f(0.0f), m_cao, true,
 		use_new_stair);
 
 	// see if we can get a little bit farther horizontally if we had
