4bb7c3d24dddcf83d9e301d3827355434a16c15b
Nauta Turbidus <88062389+nauta-turbidus@users.noreply.github.com>
2026-02-24 02:37:09 +0100

    Replaced operator[] with emplacing

--- src/content/subgames.cpp
+++ src/content/subgames.cpp
@@ -104,7 +104,10 @@ struct GameFindPath
 			path(path), user_specific(user_specific)
 	{
 	}
-	GameFindPath() { }
+	GameFindPath(const std::string &path, bool user_specific, std::unordered_set<std::string>&& aliases) :
+			path(path), user_specific(user_specific), aliases(aliases)
+	{
+	}
 };
 
 using GamePathMap = std::unordered_map<std::string, GameFindPath>;
@@ -137,8 +140,9 @@ static GamePathMap getAvailableGamePaths()
 				continue;
 
 			// Add it to result
-			(gamepaths[normalizeGameId(dln.name)] = {game_path, search_path.user_specific}).aliases
-				= getAliasesFromSettings(conf);
+			gamepaths.try_emplace(normalizeGameId(dln.name),
+				game_path, search_path.user_specific, getAliasesFromSettings(conf)
+			);
 		}
 	}
 	return gamepaths;
