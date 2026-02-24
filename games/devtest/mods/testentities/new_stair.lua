66b8b1cc6d37f52ac323d422cc5d6042216379b1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-02-23 22:47:53 -0500

    Update devtest testing

--- games/devtest/mods/testentities/new_stair.lua
+++ games/devtest/mods/testentities/new_stair.lua
@@ -1,12 +1,12 @@
--- This command tests the new_stair setting.
+-- This command tests the new_step_up setting.
 local stair_switch = false
 core.register_chatcommand("newstair", {
 	func = function(name)
 		local player = core.get_player_by_name(name)
 		if not player then return end
 		stair_switch = not stair_switch
-		player:set_properties({ new_stair = stair_switch })
-		core.chat_send_player(name, "new_stair is now: " .. tostring(stair_switch))
+		player:set_properties({ new_step_up = stair_switch })
+		core.chat_send_player(name, "new_step_up is now: " .. tostring(stair_switch))
 	end
 })
 
@@ -31,7 +31,7 @@ core.register_entity("testentities:stair_sniffer", {
 		self.object:set_acceleration(vector.new(0, -10, 0))
 		self.object:set_properties({
 			-- This setting.
-			new_stair = true,
+			new_step_up = true,
 			stepheight = 0.5,
 		})
 	end,
