a9fb6b2851364cfa9c06609ad3b4b61e86f65c41
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-02-26 19:49:57 -0500

    Swap entity test to use 3 settings

--- games/devtest/mods/testentities/new_step_up.lua
+++ games/devtest/mods/testentities/new_step_up.lua
@@ -1,11 +1,13 @@
 -- This entity helps test/showcase new_step_up for lua entities.
+local setting_cycle = {legacy = 1, floaty = 2, rigid = 3}
+local settings = {"legacy", "floaty", "rigid"}
 core.register_entity("testentities:step_up_test", {
 	initial_properties = {
 		physical = true,
 		collide_with_objects = false,
 		visual = "cube",
 		textures = { "testentities_cube1.png", "testentities_cube1.png", "testentities_cube1.png", "testentities_cube1.png", "testentities_cube1.png", "testentities_cube1.png" },
-		infotext = "Punch me to toggle new_step_up.\nnew_step_up fixes the jolt when jumping up a node.\nSetting:" .. tostring(0),
+		infotext = "Punch me to toggle new_step_up.\nnew_step_up fixes the jolt when jumping up a node.\nSetting: legacy",
 		new_step_up = 0,
 		stepheight = 0.5,
 		-- This collision box is set like this for testing.
@@ -17,8 +19,9 @@ core.register_entity("testentities:step_up_test", {
 		self.object:set_armor_groups({punch_operable = 1})
 	end,
 	on_punch = function(self)
-		local new_setting = self.object:get_properties().new_step_up + 1
-		if new_setting > 2 then new_setting = 0 end
+		local new_setting_id = setting_cycle[self.object:get_properties().new_step_up] + 1
+		if new_setting_id > 3 then new_setting_id = 1 end
+		local new_setting = settings[new_setting_id]
 		self.object:set_properties({
 			new_step_up = new_setting,
 			infotext = "Punch me to toggle new_step_up.\nnew_step_up fixes the jolt when jumping up a node.\nSetting:" .. tostring(new_setting)
