88ca76b64799fb6f08309f2cedb61d1c78253186
siliconsniffer <97843108+siliconsniffer@users.noreply.github.com>
2026-04-03 16:40:01 +0200

    Add keyboard_look_speed setting.

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -385,6 +385,7 @@ Game::Game() :
 		"repeat_place_time", "repeat_dig_time", "noclip", "free_move", "fog_start",
 		"cinematic", "cinematic_camera_smoothing", "camera_smoothing", "invert_mouse",
 		"enable_hotbar_mouse_wheel", "invert_hotbar_mouse_wheel", "pause_on_lost_focus",
+		"keyboard_look_speed",
 	};
 	for (auto s : settings)
 		g_settings->registerChangedCallback(s, &settingChangedCallback, this);
@@ -2018,7 +2019,7 @@ void Game::updateCameraOrientation(CameraOrientation *cam, float dtime)
 	const bool kb_pitch_down = input->isKeyDown(KeyType::CAMERA_PITCH_DOWN);
 
 	if (kb_yaw_left || kb_yaw_right || kb_pitch_up || kb_pitch_down) {
-		const f32 rate = 600.0f * dtime * m_cache_mouse_sensitivity * sens_scale;
+		const f32 rate = 600.0f * dtime * m_cache_keyboard_look_speed * sens_scale;
 
 		if (kb_yaw_left)
 			cam->camera_yaw += rate;
@@ -3744,6 +3745,7 @@ void Game::readSettings()
 	m_cache_enable_joysticks             = g_settings->getBool("enable_joysticks");
 	m_cache_enable_fog                   = g_settings->getBool("enable_fog");
 	m_cache_mouse_sensitivity            = g_settings->getFloat("mouse_sensitivity", 0.001f, 10.0f);
+	m_cache_keyboard_look_speed          = g_settings->getFloat("keyboard_look_speed", 0.001f, 10.0f);
 	m_cache_joystick_frustum_sensitivity = std::max(g_settings->getFloat("joystick_frustum_sensitivity"), 0.001f);
 	m_repeat_place_time                  = g_settings->getFloat("repeat_place_time", 0.16f, 2.0f);
 	m_repeat_dig_time                    = g_settings->getFloat("repeat_dig_time", 0.0f, 2.0f);
