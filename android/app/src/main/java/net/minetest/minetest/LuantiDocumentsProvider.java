a8d70b7189df0a37b12f0bc48988746142c8e068
Stvk imension <76146430+oong819@users.noreply.github.com>
2026-04-14 22:58:38 +0700

    [WIP] Update LuantiDocumentsProvider.java
    Implementing queryRoots(String[] projection)

--- android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
+++ android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
@@ -1,7 +1,19 @@
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.provider.DocumentsProvider;
+
+import java.io.FileNotFoundException;
 
 public class LuantiDocumentsProvider extends DocumentsProvider {
 
   @Override
-  public
+  public Cursor queryRoots(String[] projection) throws FileNotFoundException {
+    
+  }
+
+  // helper method
+  private MatrixCursor resolveRootProjection(String[] projection) {
+    if (projection == null) 
+  }
+	
 }
