baa1fdca439ea240aa44b08447dc7eafd7287fe5
Stvk imension <76146430+oong819@users.noreply.github.com>
2026-04-15 14:01:11 +0700

    [WIP] Update LuantiDocumentsProvider.java
    backup my changes

--- android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
+++ android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
@@ -1,19 +1,29 @@
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.provider.DocumentsProvider;
+import android.provider.DocumentsContract.Root;
 
 import java.io.FileNotFoundException;
 
 public class LuantiDocumentsProvider extends DocumentsProvider {
 
+  private static final String[] DEFAULT_ROOT_PROJECTION = new String[] {
+    Root.COLUMN_ROOT_ID, Root.COLUMN_MIME_TYPES,
+    Root.COLUMN_FLAGS, Root.COLUMN_ICON, Root.COLUMN_TITLE
+	Root.COLUMN_SUMMARY, Root.COLUMN_DOCUMENT_ID};
+	
   @Override
   public Cursor queryRoots(String[] projection) throws FileNotFoundException {
     
   }
 
   // helper method
-  private MatrixCursor resolveRootProjection(String[] projection) {
-    if (projection == null) 
+  private String[] resolveRootProjection(String[] projection) {
+    if (projection == null) {
+		return DEFAULT_ROOT_PROJECTION;
+	} else {
+		return projection;
+	}
   }
 	
 }
