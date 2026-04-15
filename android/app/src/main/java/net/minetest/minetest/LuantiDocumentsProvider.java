1f1a09ce81381863c6ae03ce3c1806eb76485f1f
Stvk imension <76146430+oong819@users.noreply.github.com>
2026-04-15 18:16:26 +0700

    Kind of finish implementing queryRoots method | UpdateuantiDocumentsProvider.java

--- android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
+++ android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
@@ -7,14 +7,25 @@ import java.io.FileNotFoundException;
 
 public class LuantiDocumentsProvider extends DocumentsProvider {
 
+  private static final String EXTERNAL_APP_DATA_DIRECTORY = Utils.getUserDataDirectory(getContext()).getAbsolutePath();
+	
   private static final String[] DEFAULT_ROOT_PROJECTION = new String[] {
     Root.COLUMN_ROOT_ID, Root.COLUMN_MIME_TYPES,
-    Root.COLUMN_FLAGS, Root.COLUMN_ICON, Root.COLUMN_TITLE
-	Root.COLUMN_SUMMARY, Root.COLUMN_DOCUMENT_ID};
+    Root.COLUMN_FLAGS, Root.COLUMN_ICON, Root.COLUMN_TITLE,
+	Root.COLUMN_DOCUMENT_ID};
 	
   @Override
   public Cursor queryRoots(String[] projection) throws FileNotFoundException {
-    
+	// Let's see what happens if I ignore projection
+    final MatrixCursor result = new MatrixCursor(DEFAULT_ROOT_PROJECTION /*resolveRootProjection(projection)*/);
+	final MatrixCursor.RowBuilder appDataRootRow = result.newRow();
+
+	appDataRootRow.add(Root.COLUMN_ROOT_ID, EXTERNAL_APP_DATA_DIRECTORY);
+	appDataRootRow.add(Root.COLUMN_MIME_TYPES, "*/*");
+	appDataRootRow.add(Root.COLUMN_FLAGS, Root.FLAG_SUPPORTS_CREATE | Root.FLAG_SUPPORTS_SEARCH);
+    appDataRootRow.add(Root.COLUMN_ICON, R.mipmap.ic_launcher);
+	appDataRootRow.add(Root.COLUMN_TITLE, getContext().getString(R.string.documents_provider_root_title));
+	appDataRootRow.add(Root.COLUMN_DOCUMENT_ID, EXTERNAL_APP_DATA_DIRECTORY);
   }
 
   // helper method
