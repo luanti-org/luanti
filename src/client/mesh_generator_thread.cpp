9ccaf4250d651bd4cc346df4ca21be1c282481c9
cx384 <cx384@proton.me>
2026-04-15 19:07:25 +0200

    Fix mapblock mesh memory leak

--- src/client/mesh_generator_thread.cpp
+++ src/client/mesh_generator_thread.cpp
@@ -263,13 +263,13 @@ void MeshUpdateWorkerThread::doUpdate()
 
 		MeshUpdateResult r;
 		r.p = q->p;
-		r.mesh = mesh_new;
+		r.mesh = std::unique_ptr<MapBlockMesh>(mesh_new);
 		r.solid_sides = get_solid_sides(q->data);
 		r.ack_list = std::move(q->ack_list);
 		r.urgent = q->urgent;
 		r.map_blocks = std::move(q->map_blocks);
 
-		m_manager->putResult(r);
+		m_manager->putResult(std::move(r));
 		m_queue_in->done(q->p);
 		delete q;
 		sp.stop();
@@ -327,12 +327,12 @@ void MeshUpdateManager::updateBlock(Map *map, v3s16 p, bool ack_block_to_server,
 	deferUpdate();
 }
 
-void MeshUpdateManager::putResult(const MeshUpdateResult &result)
+void MeshUpdateManager::putResult(MeshUpdateResult &&result)
 {
 	if (result.urgent)
-		m_queue_out_urgent.push_back(result);
+		m_queue_out_urgent.push_back(std::move(result));
 	else
-		m_queue_out.push_back(result);
+		m_queue_out.push_back(std::move(result));
 }
 
 bool MeshUpdateManager::getNextResult(MeshUpdateResult &r)
@@ -358,7 +358,6 @@ void MeshUpdateManager::clearAllQueues(bool finish)
 		for (auto *block : r.map_blocks)
 			if (block)
 				block->refDrop();
-		delete r.mesh;
 	};
 	// Same problem as in MeshUpdateQueue::clear() here: we can't just blindly
 	// throw away results that the server expects to receive an ack for.
