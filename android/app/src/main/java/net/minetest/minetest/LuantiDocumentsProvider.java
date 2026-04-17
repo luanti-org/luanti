80811e31c057fec1b518f41d91d44c625fbf6c20
Stvk imension <76146430+oong819@users.noreply.github.com>
2026-04-17 15:55:45 +0700

    Update LuantiDocumentsProvider.java

--- android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
+++ android/app/src/main/java/net/minetest/minetest/LuantiDocumentsProvider.java
@@ -1,7 +1,9 @@
 import android.database.Cursor;
 import android.database.MatrixCursor;
+import android.os.ParcelFileDescriptor;
 import android.provider.DocumentsProvider;
 import android.provider.DocumentsContract.Root;
+import android.provider.DocumentsContract.Document;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,7 +20,7 @@ public class LuantiDocumentsProvider extends DocumentsProvider {
   private static final String[] DEFAULT_DOCUMENT_PROJECTION = new String[] {
     Document.COLUMN_DOCUMENT_ID, Document.COLUMN_DISPLAY_NAME,
     Document.COLUMN_MIME_TYPE, Document.COLUMN_LAST_MODIFIED,
-    Document.COLUMN_FLAGS, Document.COLUMN_SIZE,};
+    Document.COLUMN_FLAGS, Document.COLUMN_SIZE};
 	
   @Override
   public Cursor queryRoots(String[] projection) throws FileNotFoundException {
@@ -29,7 +31,7 @@ public class LuantiDocumentsProvider extends DocumentsProvider {
 	
 	appDataRootRow.add(Root.COLUMN_ROOT_ID, EXTERNAL_APP_DATA_DIRECTORY);
     appDataRootRow.add(Root.COLUMN_MIME_TYPES, "*/*");
-	appDataRootRow.add(Root.COLUMN_FLAGS, /*Root.FLAG_SUPPORTS_CREATE |*/ Root.FLAG_SUPPORTS_SEARCH);
+	appDataRootRow.add(Root.COLUMN_FLAGS, /*Root.FLAG_SUPPORTS_CREATE |*/ 0);
     appDataRootRow.add(Root.COLUMN_ICON, R.mipmap.ic_launcher);
 	appDataRootRow.add(Root.COLUMN_TITLE, getContext().getString(R.string.documents_provider_root_title));
 	appDataRootRow.add(Root.COLUMN_DOCUMENT_ID, EXTERNAL_APP_DATA_DIRECTORY);
@@ -50,7 +52,7 @@ public class LuantiDocumentsProvider extends DocumentsProvider {
 		childDocumentRow.add(Document.COLUMN_DISPLAY_NAME, child.getName());
 		if (child.isDirectory()) {
 		  childDocumentRow.add(Document.COLUMN_MIME_TYPE, Document.MIME_TYPE_DIR);
-		else {
+		} else {
           childDocumentRow.add(Document.COLUMN_MIME_TYPE, "*/*");
 		}
 		childDocumentRow.add(Document.COLUMN_LAST_MODIFIED, child.lastModified());
@@ -60,8 +62,41 @@ public class LuantiDocumentsProvider extends DocumentsProvider {
     return result;
 }
 
+@Override
+public Cursor queryDocument(String documentId, String[] projection) throws FileNotFoundException {
+
+    final MatrixCursor result = new MatrixCursor(resolveDocumentProjection(projection));
+    final File document = new File(documentId);
+    
+        final MatrixCursor.RowBuilder documentRow = result.newRow();
+
+		documentRow.add(Document.COLUMN_DOCUMENT_ID, document.getAbsolutePath());
+		documentRow.add(Document.COLUMN_DISPLAY_NAME, document.getName());
+		if (document.isDirectory()) {
+		  documentRow.add(Document.COLUMN_MIME_TYPE, Document.MIME_TYPE_DIR);
+	    } else {
+          documentRow.add(Document.COLUMN_MIME_TYPE, "*/*");
+		}
+		documentRow.add(Document.COLUMN_LAST_MODIFIED, document.lastModified());
+		documentRow.add(Document.COLUMN_FLAGS, 0);
+		documentRow.add(Document.COLUMN_SIZE, document.length());
+    
+    return result;
+}
+
+@Override
+public ParcelFileDescriptor openDocument(final String documentId, final String mode, CancellationSignal signal) throws UnsupportedOperationException, FileNotFoundException {
+    final File file = new File(documentId);
+
+	if (mode != "r") {
+      throw UnsupportedOperationException("Only r mode (MODE_READ_ONLY) is supported");
+	}
+
+    return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+}
+	
   // Helper methods
-  private String[] resolveRootProjection(String[] projection) {
+  private static String[] resolveRootProjection(String[] projection) {
     if (projection == null) {
 		return DEFAULT_ROOT_PROJECTION;
 	} else {
@@ -69,7 +104,7 @@ public class LuantiDocumentsProvider extends DocumentsProvider {
 	}
   }
 
-  private String[] resolveDocumentProjection(String[] projection) {
+  private static String[] resolveDocumentProjection(String[] projection) {
     if (projection == null) {
 		return DEFAULT_DOCUMENT_PROJECTION;
 	} else {
