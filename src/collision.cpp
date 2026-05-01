78f71dd6de9debcb594a953419c7969f65e745d2
JosiahWI <41302989+JosiahWI@users.noreply.github.com>
2026-05-01 05:50:22 -0500

    Add f postfix
    Co-authored-by: cx384 <cx384@proton.me>

--- src/collision.cpp
+++ src/collision.cpp
@@ -700,7 +700,7 @@ CollisionMoveResult KineticObject::collideWith(Collision collision,
 		if (bounce < -1e-4 && fabsf(this->velocity.X) > BS * 3) {
 			this->velocity.X *= bounce;
 		} else {
-			this->velocity.X = 0;
+			this->velocity.X = 0.f;
 			// avoid colliding in the next iterations
 			this->accel.X = 0;
 		}
