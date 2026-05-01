e03421110795c303980f1a4e9f7b919b69c8c7e9
JosiahWI <41302989+JosiahWI@users.noreply.github.com>
2026-05-01 05:49:50 -0500

    Add f postfixes
    Co-authored-by: cx384 <cx384@proton.me>

--- src/collision.cpp
+++ src/collision.cpp
@@ -479,7 +479,7 @@ CollisionMoveResult collisionMoveSimple(Environment *env, IGameDef *gamedef,
 	// solely on loaded CONTENT_IGNORE nodes, no matter where they come from.
 	if (!locate_cboxes_in_movement_range(
 				collider, dtime, gamedef, env, cinfo)) {
-		*speed_f = v3f(0, 0, 0);
+		*speed_f = v3f(0.f, 0.f, 0.f);
 		return CollisionMoveResult{};
 	}
 
