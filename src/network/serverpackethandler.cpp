minetest/minetest

Refactor server-side node placement or dig prediction handling
The code for handling `TOSERVER_INTERACT` packets has some style-related
problems:
* There are five places with mapblock-sending code to undo predictions in
  the large `handleCommand_Interact` method in `serverpackethandler.cpp`,
  so these can be abstracted away into a function to shorten the code.
* In `networkprotocol.h`, below `TOSERVER_INTERACT`,
  there is an outdated copy of the `InteractAction` enum
  documentation and the player position information is not mentioned.
* In `serverpackethandler.cpp`, there is a (newer) copy of the
  `TOSERVER_INTERACT` documentation from `networkprotocol.h`.

Furthermore, the way the server undoes or confirms client-side node placement
or dig predictions looks inconsistent, thus erroneous:
* If a player tries to place a node in a distance too far away or if he/she
  is dead,
  only the mapblock containing `pointed.node_undersurface` is triggered to be
  sent although the player may have placed the node at
  `pointed.node_abovesurface` and this position may be in a different mapblock.
* Similarly, if a player without interact privilege tries to place a node,
  only the mapblock containing `pointed.node_abovesurface` is triggered to be
  sent although the player may have placed the node at
  `pointed.node_undersurface` and this position may be in a different mapblock.

This change addresses the problems listed above:
* Fix the documentation comments related to the TOSERVER_INTERACT package
  content
* Move mapblock sending code related to predictions to ClientInterface
* If the player tries to place but has no interact privilege, is dead,
  or exceeds the permitted distance,
  send both the mapblock corresponding to `pointed.node_undersurface` and
  `pointed.node_abovesurface` if these mapblocks differ

src/network/serverpackethandler.cpp
@@ -922,15 +922,6 @@ static inline void getWieldedItem(const PlayerSAO *playersao, std::optional<Item
 
 void Server::handleCommand_Interact(NetworkPacket *pkt)
 {
-	/*
-		[0] u16 command
-		[2] u8 action
-		[3] u16 item
-		[5] u32 length of the next item (plen)
-		[9] serialized PointedThing
-		[9 + plen] player position information
-	*/
-
 	InteractAction action;
 	u16 item_i;
 
@@ -967,13 +958,7 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 	if (playersao->isDead()) {
 		actionstream << "Server: " << player->getName()
 				<< " tried to interact while dead; ignoring." << std::endl;
-		if (pointed.type == POINTEDTHING_NODE) {
-			// Re-send block to revert change on client-side
-			RemoteClient *client = getClient(peer_id);
-			v3s16 blockpos = getNodeBlockPos(pointed.node_undersurface);
-			client->SetBlockNotSent(blockpos);
-		}
-		// Call callbacks
+		getClient(peer_id)->respondToInteraction(action, pointed, false);
 		m_script->on_cheat(playersao, "interacted_while_dead");
 		return;
 	}
@@ -1013,22 +998,7 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 	if (!checkPriv(player->getName(), "interact")) {
 		actionstream << player->getName() << " attempted to interact with " <<
 				pointed.dump() << " without 'interact' privilege" << std::endl;
-
-		if (pointed.type != POINTEDTHING_NODE)
-			return;
-
-		// Re-send block to revert change on client-side
-		RemoteClient *client = getClient(peer_id);
-		// Digging completed -> under
-		if (action == INTERACT_DIGGING_COMPLETED) {
-			v3s16 blockpos = getNodeBlockPos(pointed.node_undersurface);
-			client->SetBlockNotSent(blockpos);
-		}
-		// Placement -> above
-		else if (action == INTERACT_PLACE) {
-			v3s16 blockpos = getNodeBlockPos(pointed.node_abovesurface);
-			client->SetBlockNotSent(blockpos);
-		}
+		getClient(peer_id)->respondToInteraction(action, pointed, false);
 		return;
 	}
 
@@ -1056,12 +1026,7 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 		float d = playersao->getEyePosition().getDistanceFrom(target_pos);
 
 		if (!checkInteractDistance(player, d, pointed.dump())) {
-			if (pointed.type == POINTEDTHING_NODE) {
-				// Re-send block to revert change on client-side
-				RemoteClient *client = getClient(peer_id);
-				v3s16 blockpos = getNodeBlockPos(pointed.node_undersurface);
-				client->SetBlockNotSent(blockpos);
-			}
+			getClient(peer_id)->respondToInteraction(action, pointed, false);
 			return;
 		}
 	}
@@ -1214,14 +1179,12 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 		if (is_valid_dig && n.getContent() != CONTENT_IGNORE)
 			m_script->node_on_dig(p_under, n, playersao);
 
-		v3s16 blockpos = getNodeBlockPos(p_under);
-		RemoteClient *client = getClient(peer_id);
-		// Send unusual result (that is, node not being removed)
-		if (m_env->getMap().getNode(p_under).getContent() != CONTENT_AIR)
-			// Re-send block to revert change on client-side
-			client->SetBlockNotSent(blockpos);
-		else
-			client->ResendBlockIfOnWire(blockpos);
+		// For whatever reason we assume that the client always predicts that a
+		// dug node is air irrespective of the node's node_dig_prediction
+		bool prediction_success =
+			m_env->getMap().getNode(p_under).getContent() == CONTENT_AIR;
+		getClient(peer_id)->respondToInteraction(action, pointed,
+			prediction_success);
 
 		return;
 	} // action == INTERACT_DIGGING_COMPLETED
@@ -1265,24 +1228,11 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 				SendInventory(player, true);
 		}
 
-		if (pointed.type != POINTEDTHING_NODE)
-			return;
-
-		// If item has node placement prediction, always send the
-		// blocks to make sure the client knows what exactly happened
-		RemoteClient *client = getClient(peer_id);
-		v3s16 blockpos = getNodeBlockPos(pointed.node_abovesurface);
-		v3s16 blockpos2 = getNodeBlockPos(pointed.node_undersurface);
-		if (had_prediction) {
-			client->SetBlockNotSent(blockpos);
-			if (blockpos2 != blockpos)
-				client->SetBlockNotSent(blockpos2);
-		} else {
-			client->ResendBlockIfOnWire(blockpos);
-			if (blockpos2 != blockpos)
-				client->ResendBlockIfOnWire(blockpos2);
-		}
-
+		// Since we do not known if the client has predicted the node at
+		// pointed.above or pointed.under,
+		// we assume that a prediction is always wrong.
+		getClient(peer_id)->respondToInteraction(action, pointed,
+			!had_prediction);
 		return;
 	} // action == INTERACT_PLACE
 

luanti-org/luanti

src/network/serverpackethandler.cpp
@@ -883,15 +883,6 @@ static inline void getWieldedItem(const PlayerSAO *playersao, std::optional<Item
 
 void Server::handleCommand_Interact(NetworkPacket *pkt)
 {
-	/*
-		[0] u16 command
-		[2] u8 action
-		[3] u16 item
-		[5] u32 length of the next item (plen)
-		[9] serialized PointedThing
-		[9 + plen] player position information
-	*/
-
 	InteractAction action;
 	u16 item_i;
 
@@ -921,13 +912,7 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 	if (playersao->isDead()) {
 		actionstream << "Server: " << player->getName()
 				<< " tried to interact while dead; ignoring." << std::endl;
-		if (pointed.type == POINTEDTHING_NODE) {
-			// Re-send block to revert change on client-side
-			RemoteClient *client = getClient(peer_id);
-			v3s16 blockpos = getNodeBlockPos(pointed.node_undersurface);
-			client->SetBlockNotSent(blockpos);
-		}
-		// Call callbacks
+		getClient(peer_id)->fixupNodePrediction(action, pointed, false);
 		m_script->on_cheat(playersao, "interacted_while_dead");
 		return;
 	}
@@ -968,22 +953,7 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 	if (!checkPriv(player->getName(), "interact")) {
 		actionstream << player->getName() << " attempted to interact with " <<
 				pointed.dump() << " without 'interact' privilege" << std::endl;
-
-		if (pointed.type != POINTEDTHING_NODE)
-			return;
-
-		// Re-send block to revert change on client-side
-		RemoteClient *client = getClient(peer_id);
-		// Digging completed -> under
-		if (action == INTERACT_DIGGING_COMPLETED) {
-			v3s16 blockpos = getNodeBlockPos(pointed.node_undersurface);
-			client->SetBlockNotSent(blockpos);
-		}
-		// Placement -> above
-		else if (action == INTERACT_PLACE) {
-			v3s16 blockpos = getNodeBlockPos(pointed.node_abovesurface);
-			client->SetBlockNotSent(blockpos);
-		}
+		getClient(peer_id)->fixupNodePrediction(action, pointed, false);
 		return;
 	}
 
@@ -1011,12 +981,7 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 		float d = playersao->getEyePosition().getDistanceFrom(target_pos);
 
 		if (!checkInteractDistance(player, d, pointed.dump())) {
-			if (pointed.type == POINTEDTHING_NODE) {
-				// Re-send block to revert change on client-side
-				RemoteClient *client = getClient(peer_id);
-				v3s16 blockpos = getNodeBlockPos(pointed.node_undersurface);
-				client->SetBlockNotSent(blockpos);
-			}
+			getClient(peer_id)->fixupNodePrediction(action, pointed, false);
 			return;
 		}
 	}
@@ -1169,14 +1134,12 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 		if (is_valid_dig && n.getContent() != CONTENT_IGNORE)
 			m_script->node_on_dig(p_under, n, playersao);
 
-		v3s16 blockpos = getNodeBlockPos(p_under);
-		RemoteClient *client = getClient(peer_id);
-		// Send unusual result (that is, node not being removed)
-		if (m_env->getMap().getNode(p_under).getContent() != CONTENT_AIR)
-			// Re-send block to revert change on client-side
-			client->SetBlockNotSent(blockpos);
-		else
-			client->ResendBlockIfOnWire(blockpos);
+		// For whatever reason we assume that the client always predicts that a
+		// dug node is air irrespective of the node's node_dig_prediction
+		bool prediction_success =
+			m_env->getMap().getNode(p_under).getContent() == CONTENT_AIR;
+		getClient(peer_id)->fixupNodePrediction(action, pointed,
+			prediction_success);
 
 		return;
 	} // action == INTERACT_DIGGING_COMPLETED
@@ -1223,22 +1186,11 @@ void Server::handleCommand_Interact(NetworkPacket *pkt)
 			return;
 
 		getClient(peer_id)->m_time_from_building = 0;
-
-		// If item has node placement prediction, always send the
-		// blocks to make sure the client knows what exactly happened
-		RemoteClient *client = getClient(peer_id);
-		v3s16 blockpos = getNodeBlockPos(pointed.node_abovesurface);
-		v3s16 blockpos2 = getNodeBlockPos(pointed.node_undersurface);
-		if (had_prediction) {
-			client->SetBlockNotSent(blockpos);
-			if (blockpos2 != blockpos)
-				client->SetBlockNotSent(blockpos2);
-		} else {
-			client->ResendBlockIfOnWire(blockpos);
-			if (blockpos2 != blockpos)
-				client->ResendBlockIfOnWire(blockpos2);
-		}
-
+		// Since we do not known if the client has predicted the node at
+		// pointed.above or pointed.under,
+		// we assume that a prediction is always wrong.
+		getClient(peer_id)->fixupNodePrediction(action, pointed,
+			!had_prediction);
 		return;
 	} // action == INTERACT_PLACE
 