9aef419cd0b826300dd770e309c18e2b10973735
Jürgen Rühle <j-r@online.de>
2026-06-28 12:55:49 +0200

    Respect style guide for variable naming
    Co-authored-by: SmallJoker <SmallJoker@users.noreply.github.com>

--- src/serverenvironment.cpp
+++ src/serverenvironment.cpp
@@ -707,8 +707,8 @@ u8 ServerEnvironment::findSunlight(v3s16 pos) const
 			MapNode node = m_map->getNode(neighborPos, &is_position_ok);
 			if (!is_position_ok) {
 				// This happens very rarely because the map at currentPos is loaded
-				v3s16 blockPos = getNodeBlockPos(neighborPos);
-				m_map->emergeBlock(blockPos, false);
+				v3s16 blockpos = getNodeBlockPos(neighborPos);
+				m_map->emergeBlock(blockpos, false);
 				node = m_map->getNode(neighborPos, &is_position_ok);
 				if (!is_position_ok)
 					continue; // not generated
