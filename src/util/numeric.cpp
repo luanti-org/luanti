99abaaf89bcf99ce408e694287c8d278104992e4
Lars <larsh@apache.org>
2026-08-23 09:40:51 -0700

    fix typo

--- src/util/numeric.cpp
+++ src/util/numeric.cpp
@@ -89,7 +89,7 @@ u64 murmur_hash_64_ua(const void *key, size_t len, unsigned int seed)
 bool isBlockInSight(v3s16 blockpos_b, v3f camera_pos, v3f camera_dir,
 		f32 camera_fov, f32 range, f32 *distance_ptr)
 {
-	return isBlockInSight_ex(blockpos_b, camera_pos, camera_dir, getFovAdj(camera_fov), getFovAdj(camera_fov), range, distance_ptr);
+	return isBlockInSight_ex(blockpos_b, camera_pos, camera_dir, getFovCos(camera_fov), getFovAdj(camera_fov), range, distance_ptr);
 }
 
 bool isBlockInSight_ex(v3s16 blockpos_b, v3f camera_pos, v3f camera_dir,
