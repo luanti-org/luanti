39d1f6cde67bbbbbed1f62dcf578c8079b4afbd0
paradust7 <102263465+paradust7@users.noreply.github.com>
2026-09-08 04:05:51 -0400

    Fix compiler warnings

--- src/script/cpp_api/s_base.cpp
+++ src/script/cpp_api/s_base.cpp
@@ -474,8 +474,10 @@ void ScriptApiBase::addObjectReference(ServerActiveObject *cobj)
 
 	// object_refs[id] = object
 	auto id = cobj->getId();
-	static_assert(std::numeric_limits<decltype(id)>::min() >= INT_MIN &&
-		std::numeric_limits<decltype(id)>::max() <= INT_MAX,
+	// Ensure ID can fit into int. This works because int is signed,
+	// and ::digits does not include the sign bit.
+	static_assert(std::numeric_limits<decltype(id)>::digits <=
+		std::numeric_limits<int>::digits,
 		"ID type must fit into int argument of lua_rawseti");
 	lua_pushvalue(L, object);
 	lua_rawseti(L, objectstable, id);
