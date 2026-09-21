a1851d2442faeaaa9895c7bc075b29cadb9b8fc2
paradust7 <102263465+paradust7@users.noreply.github.com>
2026-09-21 12:36:02 -0400

    Don't process packet after it is determined to be corrupt

--- src/network/mtp/threads.cpp
+++ src/network/mtp/threads.cpp
@@ -1390,7 +1390,6 @@ SharedBuffer<u8> ConnectionReceiveThread::handlePacketType_Reliable(Channel *cha
 				<< ", seqnum: " << seqnum << std::endl;)
 
 			throw ProcessedQueued("Buffered future reliable packet");
-		} catch (AlreadyExistsException &e) {
 		} catch (IncomingDataCorruption &e) {
 			m_connection->putCommand(ConnectionCommand::disconnect_peer(peer->id));
 
@@ -1399,6 +1398,7 @@ SharedBuffer<u8> ConnectionReceiveThread::handlePacketType_Reliable(Channel *cha
 				<< ", channel: " << (channelnum & 0xFF)
 				<< ", seqnum: " << seqnum
 				<< "DROPPING CLIENT!" << std::endl;)
+			throw ProcessedSilentlyException("Dropped corrupt reliable packet");
 		}
 	}
 
